d4b4ad7a1dfe48cc45e77614d1ea449a6e10344f
Aswani25 <kumar.aswanig@gmail.com>
2024-06-30 14:03:57 +0530

    get calls

---
+++ cypress/e2e/apitestsv2/getusersv2.spec.js
@@ -0,0 +1,13 @@
+describe('get api user test', () => {
+    it('get users', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://gorest.co.in/public/v2/users',
+            headers:{
+                Authorization: 'Bearer 9f2ac5962fe57ddcb885af09b96c846c2d165ddb6cb2cebba61e91aa98400bc7'
+            }
+        }).then((res) => {
+            expect(res.status).to.eq(200);
+        });
+    });
+});
