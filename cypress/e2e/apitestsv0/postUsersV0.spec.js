405b023b3d03fff22a69be4bc70f56f7d07eacfa
Aswani25 <kumar.aswanig@gmail.com>
2024-07-05 19:38:36 +0530

    covered the chaining scenario

--- cypress/e2e/apitestsv0/postUsersV0.spec.js
+++ cypress/e2e/apitestsv0/postUsersV0.spec.js
@@ -1,87 +1,136 @@
 //Using fixture file
-const dataJson = require('../../fixtures/createUsers.json')
+const dataJson = require("../../fixtures/createUsers.json");
 
-describe('Post user Request', () => { //This test case is having hardcoded values which may fails
-    let accessToken = '1bcbe327ff1c4085cf1874ec9445831a12bb215369061f354affdc5de2cebcc7';
+describe("Post user Request", () => {
+    //This test case is having hardcoded values which may fails
+    let accessToken =
+        "1bcbe327ff1c4085cf1874ec9445831a12bb215369061f354affdc5de2cebcc7";
     let randomText = "";
     let testEmail = "";
-    it.skip('Craete user Test', () => {
+    it.skip("Craete user Test", () => {
         cy.request({
-            method: 'POST',
-            url: 'https://gorest.co.in/public/v1/users',
-            headers:{
-                'Authorization': 'Bearer ' + accessToken
+            method: "POST",
+            url: "https://gorest.co.in/public/v1/users",
+            headers: {
+                Authorization: "Bearer " + accessToken,
             },
             body: {
-                "name": "Bhaasvan Khanna",
-                "gender": "male",
-                "email": "cypresstesting3@gmail.com",
-                "status": "active"
-            }
+                name: "Bhaasvan Khanna",
+                gender: "male",
+                email: "cypresstesting3@gmail.com",
+                status: "active",
+            },
         }).then((res) => {
             cy.log(JSON.stringify(res));
             expect(res.status).to.eq(201);
-            expect(res.body.data).has.property('name','Bhaasvan Khanna');
-            expect(res.body.data).has.property('gender','male');
-            expect(res.body.data).has.property('email','cypresstesting3@gmail.com');
-            expect(res.body.data).has.property('status','active');
+            expect(res.body.data).has.property("name", "Bhaasvan Khanna");
+            expect(res.body.data).has.property("gender", "male");
+            expect(res.body.data).has.property("email", "cypresstesting3@gmail.com");
+            expect(res.body.data).has.property("status", "active");
             // expect(res.body.name).to.eq('Bhaasvan Khanna');
         });
     });
-    it('Craete user Test using random email', () => { //This test case is having dynamic values which will pass always
+    it.skip("Craete user Test using random email", () => {
+        //This test case is having dynamic values which will pass always
         var pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        for (var i = 0; i < 10; i++){
+        for (var i = 0; i < 10; i++) {
             randomText += pattern.charAt(Math.floor(Math.random() * pattern.length));
-            testEmail = randomText +'@gmail.com';
+            testEmail = randomText + "@gmail.com";
         }
         cy.request({
-            method: 'POST',
-            url: 'https://gorest.co.in/public/v1/users',
-            headers:{
-                'Authorization': 'Bearer ' + accessToken
+            method: "POST",
+            url: "https://gorest.co.in/public/v1/users",
+            headers: {
+                Authorization: "Bearer " + accessToken,
             },
             body: {
-                "name": "Bhaasvan Khanna",
-                "gender": "male",
-                "email": testEmail,
-                "status": "active"
-            }
+                name: "Bhaasvan Khanna",
+                gender: "male",
+                email: testEmail,
+                status: "active",
+            },
         }).then((res) => {
             cy.log(JSON.stringify(res));
             expect(res.status).to.eq(201);
-            expect(res.body.data).has.property('name','Bhaasvan Khanna');
-            expect(res.body.data).has.property('gender','male');
-            expect(res.body.data).has.property('email',testEmail);
-            expect(res.body.data).has.property('status','active');
+            expect(res.body.data).has.property("name", "Bhaasvan Khanna");
+            expect(res.body.data).has.property("gender", "male");
+            expect(res.body.data).has.property("email", testEmail);
+            expect(res.body.data).has.property("status", "active");
             // expect(res.body.name).to.eq('Bhaasvan Khanna');
         });
     });
-    it.skip('Craete user Test using fixture file', () => { //This test case is having hardcoded values on fixture file which may fails
+    it.skip("Craete user Test using fixture file", () => {
+        //This test case is having hardcoded values on fixture file which may fails
         var pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        for (var i = 0; i < 10; i++){
+        for (var i = 0; i < 10; i++) {
             randomText += pattern.charAt(Math.floor(Math.random() * pattern.length));
-            testEmail = randomText +'@gmail.com';
+            testEmail = randomText + "@gmail.com";
         }
         cy.request({
-            method: 'POST',
-            url: 'https://gorest.co.in/public/v1/users',
-            headers:{
-                'Authorization': 'Bearer ' + accessToken
+            method: "POST",
+            url: "https://gorest.co.in/public/v1/users",
+            headers: {
+                Authorization: "Bearer " + accessToken,
             },
             body: {
-                "name": dataJson.name,
-                "gender": dataJson.gender,
-                "email": dataJson.email,
-                "status": dataJson.status
-            }
+                name: dataJson.name,
+                gender: dataJson.gender,
+                email: dataJson.email,
+                status: dataJson.status,
+            },
         }).then((res) => {
             cy.log(JSON.stringify(res));
             expect(res.status).to.eq(201);
-            expect(res.body.data).has.property('name',dataJson.name);
-            expect(res.body.data).has.property('gender',dataJson.gender);
-            expect(res.body.data).has.property('email',dataJson.email);
-            expect(res.body.data).has.property('status',dataJson.status);
+            expect(res.body.data).has.property("name", dataJson.name);
+            expect(res.body.data).has.property("gender", dataJson.gender);
+            expect(res.body.data).has.property("email", dataJson.email);
+            expect(res.body.data).has.property("status", dataJson.status);
             // expect(res.body.name).to.eq(dataJson.name);
+        });
+    });
+    it("Craete user Test using random email with chaining concept", () => {
+        //This test case is having dynamic values which will pass always
+        var pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        for (var i = 0; i < 10; i++) {
+            randomText += pattern.charAt(Math.floor(Math.random() * pattern.length));
+            testEmail = randomText + "@gmail.com";
+        }
+        // Create User (POST)
+        cy.request({
+            method: "POST",
+            url: "https://gorest.co.in/public/v1/users",
+            headers: {
+                Authorization: "Bearer " + accessToken,
+            },
+            body: {
+                name: "Bhaasvan Khanna",
+                gender: "male",
+                email: testEmail,
+                status: "active",
+            },
+        }).then((res) => {
+            cy.log(JSON.stringify(res));
+            expect(res.status).to.eq(201);
+            expect(res.body.data).has.property("name", "Bhaasvan Khanna");
+            expect(res.body.data).has.property("gender", "male");
+            expect(res.body.data).has.property("email", testEmail);
+            expect(res.body.data).has.property("status", "active");
+            
+            const userId = res.body.data.id; //Store the ID of the created user
+            cy.log("User ID: " + userId);
+            //Perform further actions with the user ID, if needed
+
+            // Get user (GET)
+            cy.request({
+                method: "GET",
+                url: `https://gorest.co.in/public/v1/users/${userId}`,
+                headers: {
+                    Authorization: "Bearer " + accessToken,
+                },
+            }).then((res) => {
+                cy.log(JSON.stringify(res));
+                expect(res.status).to.eq(200);
+            });
         });
     });
 });
--- cypress/e2e/apitestsv2/postUsersV2.spec.js
+++ cypress/e2e/apitestsv2/postUsersV2.spec.js
@@ -5,6 +5,7 @@ describe('Post user Request', () => {
     let accessToken = '1bcbe327ff1c4085cf1874ec9445831a12bb215369061f354affdc5de2cebcc7';
     let randomText = "";
     let testEmail = "";
+    let userId = "";
     it.skip('Craete user Test', () => {  //This test case is having hardcoded values which may fails
         cy.request({
             method: 'POST',
@@ -28,7 +29,7 @@ describe('Post user Request', () => {
             // expect(res.body.name).to.eq('Bhaasvan Khanna');
         });
     });
-    it('Craete user Test using random email', () => { //This test case is having dynamic values which will pass always
+    it.skip('Craete user Test using random email', () => { //This test case is having dynamic values which will pass always
         var pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         for (var i = 0; i < 10; i++){
             randomText += pattern.charAt(Math.floor(Math.random() * pattern.length));
@@ -81,7 +82,54 @@ describe('Post user Request', () => {
             expect(res.body).has.property('gender',dataJson.gender);
             expect(res.body).has.property('email',dataJson.email);
             expect(res.body).has.property('status',dataJson.status);
-            // expect(res.body.name).to.eq(dataJson.name);
+            // expect(res.body.name).to.eq(dataJson.name);           
+        });
+    });
+    it('Craete user Test using random email', () => { //This test case is having dynamic values which will pass always
+        var pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        for (var i = 0; i < 10; i++){
+            randomText += pattern.charAt(Math.floor(Math.random() * pattern.length));
+            testEmail = randomText +'@gmail.com';
+        }
+        cy.request({
+            method: 'POST',
+            url: 'https://gorest.co.in/public/v2/users',
+            headers:{
+                'Authorization': 'Bearer ' + accessToken
+            },
+            body: {
+                "name": "Bhaasvan Khanna",
+                "gender": "male",
+                "email": testEmail,
+                "status": "active"
+            }
+        }).then((res) => {
+            cy.log(JSON.stringify(res));
+            expect(res.status).to.eq(201);
+            expect(res.body).has.property('name','Bhaasvan Khanna');
+            expect(res.body).has.property('gender','male');
+            expect(res.body).has.property('email',testEmail);
+            expect(res.body).has.property('status','active');
+            
+            userId = res.body.id; //Store the ID of the created user
+            cy.log("User ID: " + userId);
+            //Perform further actions with the user ID, if needed
+
+            // Get user (GET)
+            cy.request({
+                method: "GET",
+                url: `https://gorest.co.in/public/v2/users/${userId}`,
+                headers: {
+                    Authorization: "Bearer " + accessToken,
+                },
+            }).then((res) => {
+                cy.log(JSON.stringify(res));
+                expect(res.status).to.eq(200);
+                expect(res.body).has.property('name','Bhaasvan Khanna');
+                expect(res.body).has.property('gender','male');
+                expect(res.body).has.property('email',testEmail);
+                expect(res.body).has.property('status','active');
+            });
         });
     });
 });
